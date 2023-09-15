d2f94cd9c2a5b1520ec8a2ce28f385b9ec427ed2
Hritik Pathania <61740445+hritikp00@users.noreply.github.com>
2023-09-15 23:02:56 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "subscription_id" {
+  description = "id"
+  type        = string
+}
+
+variable "client_id" {
+  description = "id"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "id"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "id"
+  type        = string
+}
+
+
+
+
+
